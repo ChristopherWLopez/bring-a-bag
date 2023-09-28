757fefc513a21b763466936bf6c5f0e38a6552ab
christopher lopez <christopherwlopez89@gmail.com>
2023-09-27 17:46:52 -0700

    useEffect function

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import './App.css';
 function App() {
 
   const [location, setLocation] =useState(null);
+  const[error, setError] = useState(null);
 
   useEffect(()=>{
     if("geolocation" in navigator){
       navigator.geolocation.getCurrentPosition(
         (position)=>{
-
-    }
+          const{ latitude, longitude} = position.coords;
+          setLocation({ latitude, longitude });
+    },
+  (err)=>{
+    setError(err.message);
   }
-  })
+  );
+}else{
+  setError("Geolocation is not available in your browser");
+}
   return (
     <div className="App">
       <header className="App-header">
